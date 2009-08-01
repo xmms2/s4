a68ad2ecb1453809a30e45f2ecee441d1235da86
Sivert Berg <sivertb@stud.ntnu.no>
2009-08-01 22:33:45 +0200

    Add more tests
    Added tests to test the new verification and recovery code.

---
+++ tests/s4/t_s4.c
@@ -0,0 +1,125 @@
+#include "xcu.h"
+#include "s4.h"
+#include <stdio.h>
+#include <glib.h>
+#include <unistd.h>
+
+SETUP (s4) {
+	if (!g_thread_get_initialized ())
+		g_thread_init (NULL);
+
+	return 0;
+}
+
+CLEANUP () {
+	return 0;
+}
+
+const char *name;
+s4_t *s4;
+
+static void _open ()
+{
+	name = tmpnam (NULL);
+	s4 = s4_open (name, S4_VERIFY | S4_SYNC_THREAD);
+}
+static void _close ()
+{
+	s4_close (s4);
+	unlink (name);
+}
+
+#define ARG_SIZE 10
+struct db_struct {
+	const char *name;
+	const char *args[ARG_SIZE];
+};
+
+static void create_db (struct db_struct *db)
+{
+	s4_entry_t *entry, *prop;
+	int i, j;
+
+	for (i = 0; db[i].name != NULL; i++) {
+		entry = s4_entry_get_s (s4, "entry", db[i].name);
+
+		for (j = 0; db[i].args[j] != NULL; j++) {
+			prop = s4_entry_get_s (s4, "property", db[i].args[j]);
+			s4_entry_add (s4, entry, prop, "testsuite");
+			s4_entry_free (prop);
+		}
+		s4_entry_free (entry);
+	}
+}
+
+static void check_db (struct db_struct *db)
+{
+	s4_entry_t *entry;
+	s4_set_t *set;
+	int i, j;
+
+	for (i = 0; db[i].name != NULL; i++) {
+		char *args[ARG_SIZE];
+		memcpy (args, db[i].args, ARG_SIZE * sizeof (char*));
+
+		entry = s4_entry_get_s (s4, "entry", db[i].name);
+		set = s4_entry_contains (s4, entry);
+
+		for (; set != NULL; set = s4_set_next (set)) {
+
+			s4_entry_fillin (s4, &set->entry);
+			for (j = 0; args[j] != NULL &&
+					strcmp (args[j], set->entry.val_s); j++);
+			CU_ASSERT_PTR_NOT_NULL (args[j]);
+			memmove (args + j, args + j + 1,
+					(ARG_SIZE - j - 1) * sizeof (char*));
+		}
+
+		CU_ASSERT_PTR_NULL (args[0]);
+
+		s4_entry_free (entry);
+	}
+}
+
+CASE (s4_verify) {
+	struct db_struct db[] = {
+		{"a",  {"b", "c", NULL}},
+		{"c", {"d", "e", NULL}},
+		{NULL, {NULL}}};
+	_open ();
+
+	create_db (db);
+	check_db (db);
+
+	s4_sync (s4);
+	CU_ASSERT_TRUE (s4_verify (s4, S4_VERIFY_THOROUGH | S4_VERIFY_REFCOUNT));
+
+	_close ();
+}
+
+CASE (s4_recover) {
+	char *name;
+	s4_t *tmp;
+	struct db_struct db[] = {
+		{"a",  {"b", "c", NULL}},
+		{"c", {"d", "e", NULL}},
+		{NULL, {NULL}}};
+	_open ();
+
+	create_db (db);
+	check_db (db);
+
+	name = tmpnam (NULL);
+	s4_recover (s4, name);
+
+	tmp = s4;
+	s4 = s4_open (name, S4_EXISTS);
+	s4_sync (s4);
+	CU_ASSERT_TRUE (s4_verify (s4, S4_VERIFY_THOROUGH | S4_VERIFY_REFCOUNT));
+	check_db (db);
+
+	s4_close (s4);
+	s4 = tmp;
+
+	_close();
+}
